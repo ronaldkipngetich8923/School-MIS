19737ce5b5ccfadedc52f4c2db5d68e09cad19a0
Collins Tanki <collinstanki@gmail.com>
2021-06-15 15:00:15 +0300

    Added formw

--- Shule/formw.Designer.cs
+++ Shule/formw.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // formw
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(960, 450);
+            this.Name = "formw";
             this.Text = "formw";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
